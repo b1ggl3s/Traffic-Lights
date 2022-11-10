c993b9d72a2f114b01c9386f69ec99673a6cb400
b1ggl3s <98510771+b1ggl3s@users.noreply.github.com>
2022-11-10 17:17:48 +0800

    Made classes for traffic objects

--- trafficlights.py
+++ trafficlights.py
@@ -1,32 +1,48 @@
 import queue
 
-#test
 
 
-class Intersection:
+class Vertex:
 
-    def __init__(self, inN, inE, inS, inW, outN, outE, outS, outE, strategies):
-        self.inN = inN
-        self.inE = inE
-        self.inS = inS
-        self.inW = inW
-        self.outN = outN
-        self.outE = outE
-        self.outW = outW
-        self.outS = outS
-        self.strategies = [((inN, outS), (inS, outN)),
+    def __init__(self, inAdjacencies, outAdjacencies):
+        self.inAdjacencies = inAdjacencies
+        self.outAdjacencies = outAdjacencies
+
+class Intersection(Vertex):
+
+    def getStrategies(self):
+        (inN, inE, inS, inW) = (vertex for vertex in self.inAdjacencies)
+        (outN, outE, outS, outW) = (vertex for vertex in self.outAdjacencies)
+
+        strategies = [((inN, outS), (inS, outN)),
                            ((inW, outE), (inE, outW)),
                            ((inN, outE), (inS, outW)),
                            ((inE, outS), (inW, outN)),
                            ((inN, outW), (inS, outE)),
                            ((inE, outN), (inW, outS))]
+        strategies = self.removeNullStrategies(strategies)
+        return strategies
+
+    def removeNullStrategies(self, strategies):
+        return [strategy for strategy in strategies if None not in strategy[0] and None not in strategy[1]]
+        
 
     def update(self):
         return
 
-    
+class RoadQueue:
+    def __init__(self, destinationVertex):
+        self.destinationVertex = destinationVertex
+        self.q = queue.Queue()
 
 class Road:
+
+    def __init__(self, inVertex, outVertex):
+        self.inVertex = inVertex
+        self.outVertex = outVertex
+
+    def createQueues(self):
+        [RoadQueue(vertex) for vertex in self.outVertex.outAdjacencies]
 
 
 class Car:
@@ -34,6 +50,7 @@ class Car:
     def __init__(self, destination, redTime):
         self.destination = destination
         self.redTime = redTime
+        self.instructionStack = []
 
     
 
